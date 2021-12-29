b6ca3b04f9ee2e6643237a977e1d61397084887a
Nicolas <nicolas.pino.bello@gmail.com>
2021-12-29 13:53:21 -0300

    commit initial develop

--- app/src/main/java/com/disin/operaciones/MainActivity.kt
+++ app/src/main/java/com/disin/operaciones/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //this a master
+        //this a develop
     }
 }
