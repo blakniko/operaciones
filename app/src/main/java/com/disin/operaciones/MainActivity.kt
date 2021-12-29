b0cca690c69937251eb8f7df621f1d1464b1d890
Nicolas <nicolas.pino.bello@gmail.com>
2021-12-29 13:41:48 -0300

    commit initial master

---
+++ app/src/main/java/com/disin/operaciones/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.disin.operaciones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //this a master
+    }
+}
